1cf1792b17accf580641d6e7b3365f80ab84a127
Hsueh-Xue <867997466@qq.com>
2021-07-04 16:17:38 +0800

    项目名称

--- app/urls.py
+++ app/urls.py
@@ -1,4 +1,4 @@
-"""view_oj_for_hznu_backend URL Configuration
+"""app URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
